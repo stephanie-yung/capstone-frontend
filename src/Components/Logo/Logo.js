87eba3fbb1e6b2b8cf757b572c3a7cd8bf042e32
stephanie-yung <stephanie.yung43@myhunter.cuny.edu>
2021-10-02 20:25:43 -0400

    feat: modify logo css

--- src/Components/Logo/Logo.js
+++ src/Components/Logo/Logo.js
@@ -5,9 +5,9 @@ import star from './coffee.png';
 
 const Logo = () => {
     return(
-        <div className = 'ma4 mt0'>
-            <Tilt className = "Tilt br2 shadow-2"options={{ max : 55 }} style={{ height: 150, width: 150 }} >
-                <div className = "Tilt-inner" pa3>
+        <div>
+            <Tilt style={{ height: 150, width: 150 }} >
+                <div>
                     <img style={{paddingTop:'20px'}} src={star} alt='logo'/>
                 </div>
             </Tilt>
