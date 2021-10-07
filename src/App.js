5834a1cab92f5e1e18a70a45901adea7042c8ba6
Sony <1sony9905@gmail.com>
2021-10-07 12:36:48 -0400

    resolving merge conflict, added card and installed react-tilt

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Logo from './Components/Logo/Logo';
 import './App.css';
+import Card from './Components/Card/Card';
 function App() {
   return (
     <div className="App">
       <div>
             <Logo/>
+            <Card/>
       </div>
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
