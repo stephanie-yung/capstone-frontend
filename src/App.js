f381d27b47dd5a62612c42d08399ef4b37eed805
Sony <1sony9905@gmail.com>
2021-10-11 16:10:32 -0400

    i added th sign in, got some errors that I need to fix

--- src/App.js
+++ src/App.js
@@ -5,11 +5,45 @@ import Navbar from './Components/Navbar/Navbar';
 import {BrowserRouter as Router, Switch, Route, Link, Redirect, withRouter} from "react-router-dom";
 import './App.css';
 import { render } from '@testing-library/react';
+import Navigation from './Components/Navigation/Navigation';
+import Logo from './Components/Logo/Logo';
+import Signin from './Components/SignIn/SignIn';
+import Register from './Components/Register/Register';
+import Card from './Components/Card/Card';
 
 class App extends Component{
+  constructor(){
+    super();
+    this.state = {
+      route: 'signin',
+      isSignedIn: false
+    }
+  }
+
+
+   onRouteChange = (route) => {
+     if (route ==='signout') {
+       this.setState({isSignedIn : false})
+     }else if (route === 'home') {
+       this.setState({isSignedIn:true})
+     }
+    this.setState({route: route});
+   }
   render(){
     return (
       <div className="App">
+        <Navigation isSignedIn={this.state.isSignedIn} onRouteChange={this.onRouteChange}/>
+        {this.state.route === 'home' ?
+          <div>
+            <Logo/>
+            <Card/>
+          </div> : 
+          (
+            this.state.route ==='signin' ?
+             <Signin onRouteChange={this.onRouteChange}/>
+            :<Register onRouteChange={this.onRouteChange} />
+          )
+        }
         <Router>
           <Navbar/>
           <Switch>
