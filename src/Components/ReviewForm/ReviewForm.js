df986f88ea4132617bec2ec697504517369d4c84
stephanie-yung <stephanie.yung43@myhunter.cuny.edu>
2021-10-23 13:32:52 -0400

    drink review form page

---
+++ src/Components/ReviewForm/ReviewForm.js
@@ -0,0 +1,80 @@
+import React from "react";
+import "./ReviewForm.css";
+import {FaStar} from "react-icons/fa";
+
+const colors = {
+    orange: "#FFBA5A",
+    grey: "a9a9a9"
+}
+
+function ReviewForm(){
+    const stars = Array(10).fill(0);
+    const [currentValue, setCurrentValue] = React.useState(0);
+    const [hoverValue, setHoverValue] = React.useState(undefined);
+
+    const handleClick = value =>{
+        setCurrentValue(value)
+    };
+
+    const handleMouseOver = value =>{
+        setHoverValue(value)
+    };
+
+    const handleMouseLeave = () =>{
+        setHoverValue(undefined)
+    };
+
+    return(
+        <div style={styles.container}>
+            <h1> Drink Review Form</h1>
+            <div style = {styles.stars}>
+                {stars.map((_, index) =>{
+                    return(
+                        <FaStar 
+                            key = {index}
+                            size = {24}
+                            style ={{
+                                marginRight:10,
+                                cursor: "pointer"
+                            }}
+                            color = {(hoverValue || currentValue)> index ? colors.orange: colors.grey}
+                            onClick = {() => handleClick(index+1)}
+                            onMouseOver={() => handleMouseOver(index+1)}
+                            onMouseLeave={handleMouseLeave}
+                        />
+                    )
+                })}
+            </div>
+            <textarea 
+                placeholder="What's your feedback?"
+                style={styles.textarea}
+            />
+            <button className="mousepointer" style={styles.button}>Submit</button>
+        </div>
+    );
+};
+
+const styles = {
+    container: {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+    },
+    textarea:{
+        border: "1px solid #a9a9a9",
+        borderRadius: 5,
+        width: 500,
+        padding: 10,
+        margin: "20px 0",
+        minHeight: 100,
+
+    },
+    button:{
+        border: "1px solid #a9a9a9",
+        borderRadius: 5,
+        width: 500,
+        padding: 10,
+    }
+}
+
+export default ReviewForm;
