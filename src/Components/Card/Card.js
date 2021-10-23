a0d7bcdbc6044635a3eade31058f90a1d6f7637e
stephanie-yung <stephanie.yung43@myhunter.cuny.edu>
2021-10-23 12:44:15 -0400

    changed card href

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -5,7 +5,7 @@ export const Card = () => {
         <div>
             <div className="cf pa5">
                 <div className="fl w-50 w-25-m w-20-l pa2">
-                <a href="" className="db link  tc">
+                <a href="/drinkreview" className="db link  tc">
                     <img src="https://media.gettyimages.com/photos/customer-is-using-a-paper-straw-to-drink-a-cold-starbucks-latte-to-picture-id1234106216?s=2048x2048" alt="Frank Ocean Blonde Album Cover" className="w-100 db outline black-10"/>
                     <dl className="mt2 f6 lh-copy">
                     <dt className="clip">Title</dt>
@@ -16,7 +16,7 @@ export const Card = () => {
                 </a>
                 </div>
                 <div className="fl w-50 w-25-m w-20-l pa2">
-                <a href="" className="db link  tc">
+                <a href="/drinkreview" className="db link  tc">
                     <img src="https://www.tasteofhome.com/wp-content/uploads/2021/03/starbucks-new-beverages-QT-1200x800.jpg" alt="Young Thug - Jeffery Album Cover" className="w-100 db outline black-10"/>
                     <dl className="mt2 f6 lh-copy">
                     <dt className="clip">Title</dt>
@@ -27,7 +27,7 @@ export const Card = () => {
                 </a>
                 </div>
                 <div className="fl w-50 w-25-m w-20-l pa2">
-                <a href="" className="db link  tc">
+                <a href="/drinkreview" className="db link  tc">
                     <img src="https://cdn.vox-cdn.com/thumbor/7D_2G8K0bZGrWOPCemXhv43HI8U=/234x0:1146x684/1200x800/filters:focal(234x0:1146x684)/cdn.vox-cdn.com/uploads/chorus_image/image/49962291/coffee-jelly-frappuccino.0.0.jpg" alt="Kendrick Lamar untitled unmastered. Album Cover" className="w-100 db outline black-10"/>
                     <dl className="mt2 f6 lh-copy">
                     <dt className="clip">Title</dt>
@@ -38,7 +38,7 @@ export const Card = () => {
                 </a>
                 </div>
                 <div className="fl w-50 w-25-m w-20-l pa2">
-                <a href="" className="db link  tc">
+                <a href="/drinkreview" className="db link  tc">
                     <img src="https://www.tasteofhome.com/wp-content/uploads/2021/06/starbucks-discontinued-drinks-QT-1200x800-1.jpg" alt="Kaytranada 99%" className="w-100 db outline black-10"/>
                     <dl className="mt2 f6 lh-copy">
                     <dt className="clip">Title</dt>
@@ -49,7 +49,7 @@ export const Card = () => {
                 </a>
                 </div>
                 <div className="fl w-50 w-25-m w-20-l pa2">
-                <a href="" className="db link  tc">
+                <a href="/drinkreview" className="db link  tc">
                     <img src="https://www.foodbusinessnews.net/ext/resources/2019/12/StarbucksColdFoam_Lead.jpg?1575476559" alt="Baauer Aa - Album Cover" className="w-100 db outline black-10"/>
                     <dl className="mt2 f6 lh-copy">
                     <dt className="clip">Title</dt>
@@ -60,7 +60,7 @@ export const Card = () => {
                 </a>
                 </div>
                 <div className="fl w-50 w-25-m w-20-l pa2">
-                <a href="https://geo.itunes.apple.com/us/album/99-cents/id1054734475?at=1l3vqFJ&mt=1&app=music" className="db link  tc">
+                <a href="/drinkreview" className="db link  tc">
                     <img src="https://cdn.vox-cdn.com/thumbor/z2Oc0Qtvm3Iu1WoHOgohXbX1Ncc=/0x0:5860x4008/1200x800/filters:focal(3075x2118:4011x3054)/cdn.vox-cdn.com/uploads/chorus_image/image/66490225/shutterstock_1410002591.0.jpg" alt="Santigold 99 cents - Album cover" className="w-100 db outline black-10"/>
                     <dl className="mt2 f6 lh-copy">
                     <dt className="clip">Title</dt>
@@ -71,7 +71,7 @@ export const Card = () => {
                 </a>
                 </div>
                 <div className="fl w-50 w-25-m w-20-l pa2">
-                <a href="https://geo.itunes.apple.com/us/album/a-moon-shaped-pool/id1111577743?at=1l3vqFJ&mt=1&app=music" className="db link  tc">
+                <a href="/drinkreview" className="db link  tc">
                     <img src="https://pbs.twimg.com/media/DDR2SByU0AABo_E.jpg" alt="A Moon Shaped Pool - Album cover" className="w-100 db outline black-10"/>
                     <dl className="mt2 f6 lh-copy">
                     <dt className="clip">Title</dt>
@@ -82,7 +82,7 @@ export const Card = () => {
                 </a>
                 </div>
                 <div className="fl w-50 w-25-m w-20-l pa2">
-                <a href="https://geo.itunes.apple.com/us/album/the-life-of-pablo/id1123231885?at=1l3vqFJ&mt=1&app=music" className="db link  tc">
+                <a href="/drinkreview" className="db link  tc">
                     <img src="https://www.foodbusinessnews.net/ext/resources/2021/7/StarbucksCoffeeMuffins_Lead.jpg?1625232392" alt="The Life of Pablo - Album Cover" className="w-100 db outline black-10"/>
                     <dl className="mt2 f6 lh-copy">
                     <dt className="clip">Title</dt>
@@ -93,7 +93,7 @@ export const Card = () => {
                 </a>
                 </div>
                 <div className="fl w-50 w-25-m w-20-l pa2">
-                <a href="https://geo.itunes.apple.com/us/album/coloring-book/id1113239004?at=1l3vqFJ&mt=1&app=music" className="db link  tc">
+                <a href="/drinkreview" className="db link  tc">
                     <img src="https://images.squarespace-cdn.com/content/v1/5d1a9cd8fe0ea60001050dd6/1562363453679-YQI134OSOOCWOFTNXONT/5cab4cbfe278c.image.jpg" alt="Coloring Book - Album cover" className="w-100 db outline black-10"/>
                     <dl className="mt2 f6 lh-copy">
                     <dt className="clip">Title</dt>
@@ -104,7 +104,7 @@ export const Card = () => {
                 </a>
                 </div>
                 <div className="fl w-50 w-25-m w-20-l pa2">
-                <a href="https://geo.itunes.apple.com/us/album/everybody-looking/id1135576036?at=1l3vqFJ&mt=1&app=music" className="db link  tc">
+                <a href="/drinkreview" className="db link  tc">
                     <img src="https://www.tasteofhome.com/wp-content/uploads/2020/08/starbucks-peanut-butter-cup-frappuccino-QT-1200x800.jpg" alt="Everybody Looking - Album Cover" className="w-100 db outline black-10"/>
                     <dl className="mt2 f6 lh-copy">
                     <dt className="clip">Title</dt>
