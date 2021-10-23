d4e9a15459fa0acc2b1f12afead163a7b37826af
stephanie-yung <stephanie.yung43@myhunter.cuny.edu>
2021-10-23 15:35:27 -0400

    adjust card margin

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,16 +1,16 @@
 import React from 'react'
+import { FaCentercode } from 'react-icons/fa';
 import {BrowserRouter as Router, Switch, Route, Link, Redirect, withRouter} from "react-router-dom";
 
 {/* <a href="/drinkreview" className="db link tc"></a> */}
 const Card = ({name, img, username}) => {
     const myStyle = {
         maxWidth:'50%',
-        height: '50%'
+        height: '50%',
     }
     return (
         <Link to="/drinkreview" className="db link tc">
-            <div className ='tc bg-light-blue dib br1 fl w-30 pa2 ma2 grow bw2 shadow-5'>
-                {/* <Link to="/drinkreview" className="db link tc"> */}
+            <div className ='tc bg-light-blue dib br1 fl w-30 pa2 ma3 grow bw2 shadow-5'>
                 <img src= {img} style = {myStyle}alt=''></img>
                 <div>
                     <h2>{name}</h2>
