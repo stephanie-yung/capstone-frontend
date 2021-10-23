21324024cc243a9965da7db583df484e2767d3a9
stephanie-yung <stephanie.yung43@myhunter.cuny.edu>
2021-10-23 15:29:44 -0400

    fixed card routing err to drinkreview

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import {BrowserRouter as Router, Switch, Route, Link, Redirect, withRouter} from "react-router-dom";
+
 {/* <a href="/drinkreview" className="db link tc"></a> */}
 const Card = ({name, img, username}) => {
     const myStyle = {
@@ -6,14 +8,16 @@ const Card = ({name, img, username}) => {
         height: '50%'
     }
     return (
-        <div className ='tc bg-light-blue dib br1 fl w-30 pa2 ma2 grow bw2 shadow-5'>
-            <a href="/drinkreview" className="db link tc"></a>
-            <img src= {img} style = {myStyle}alt=''></img>
-            <div>
-                <h2>{name}</h2>
-                <p>{username}</p>
+        <Link to="/drinkreview" className="db link tc">
+            <div className ='tc bg-light-blue dib br1 fl w-30 pa2 ma2 grow bw2 shadow-5'>
+                {/* <Link to="/drinkreview" className="db link tc"> */}
+                <img src= {img} style = {myStyle}alt=''></img>
+                <div>
+                    <h2>{name}</h2>
+                    <p>{username}</p>
+                </div>
             </div>
-        </div>
+        </Link>
     )
 }
 
