99d9b1aa7dc6aed1d06de156d4eabc61531736db
JW <wenroujing@hotmail.com>
2021-10-31 22:58:22 -0400

    Adding touch on nave bar and homepage

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -12,7 +12,7 @@ const Card = ({name, img, username, id}) => {
         <Link to= {{
             pathname: `/capstone-frontend/drinkreview/${id}`
         }} className="db link tc">
-            <div className ='tc bg-light-blue dib br1 fl w-30 pa2 ma3 grow bw2 shadow-5'>
+            <div style={{background: '#1f3933', color: '#fff', padding: '20px', textAlign:'left'}}    className ='tc bg-light-blue dib br1 fl w-30 pa2 ma3 grow bw2 shadow-5'>
                 <img src= {img} style = {myStyle}alt=''></img>
                 <div>
                     <h2>{name}</h2>
--- src/Components/Coffee/Coffee.js
+++ src/Components/Coffee/Coffee.js
@@ -4,70 +4,70 @@ export const coffeeInfo = [
       name: 'latte',
       username: 'Sony',
       Rating: '5',
-      img: 'https://www.tasteofhome.com/wp-content/uploads/2021/06/starbucks-discontinued-drinks-QT-1200x800-1.jpg'
+      img: 'https://globalassets.starbucks.com/assets/edd75880341c437ab194c14835fbb24e.jpg'
     },
     {
       id: 2,
       name: 'Frap',
       username: 'Thammie',
       Rating: '5',
-      img: 'https://www.tasteofhome.com/wp-content/uploads/2021/03/starbucks-new-beverages-QT-1200x800.jpg'
+      img: 'https://globalassets.starbucks.com/assets/7fe2123728f844849ae4f9ea01473468.jpg'
     },
     {
       id: 3,
       name: 'S',
       username: 'Samantha',
       Rating: '5',
-      img: 'https://www.tasteofhome.com/wp-content/uploads/2021/06/starbucks-discontinued-drinks-QT-1200x800-1.jpg'
+      img: 'https://globalassets.starbucks.com/assets/55083489399e435e81cf41d899051a5a.jpg'
     },
     {
       id: 4,
       name: 'X',
       username: 'Karianne',
       Rating: '5',
-      img: 'https://www.tasteofhome.com/wp-content/uploads/2021/06/starbucks-discontinued-drinks-QT-1200x800-1.jpg'
+      img: 'https://globalassets.starbucks.com/assets/9d72035353344b59938d007451754db5.jpg'
     },
     {
       id: 5,
       name: 'Y',
       username: 'Kamren',
       Rating: '5',
-      img: 'https://www.tasteofhome.com/wp-content/uploads/2021/06/starbucks-discontinued-drinks-QT-1200x800-1.jpg'
+      img: 'https://globalassets.starbucks.com/assets/2e2875445811469dac73223e31de6103.jpg'
     },
     {
       id: 6,
       name: 'T',
       username: 'Leopoldo_Corkery',
       Rating: '5',
-      img: 'https://www.tasteofhome.com/wp-content/uploads/2021/06/starbucks-discontinued-drinks-QT-1200x800-1.jpg'
+      img: 'https://globalassets.starbucks.com/assets/e4d9e996eb64453eb3ac7adb570c9b7b.jpg'
     },
     {
       id: 7,
-      name: 'Z',
+      name: 'Cinnamon Dolce Latte',
       username: 'Elwyn.Skiles',
       Rating: '5',
-      img: 'https://www.tasteofhome.com/wp-content/uploads/2021/06/starbucks-discontinued-drinks-QT-1200x800-1.jpg'
+      img: 'https://globalassets.starbucks.com/assets/9329c03ce24f4623be25dd24d7537db2.jpg'
     },
     {
       id: 8,
       name: 'Coffee',
       username: 'Maxime_Nienow',
       Rating: '5',
-      img: 'https://www.tasteofhome.com/wp-content/uploads/2021/06/starbucks-discontinued-drinks-QT-1200x800-1.jpg'
+      img: 'https://globalassets.starbucks.com/assets/fedee22e49724cd09fbcc7ee2e567377.jpg'
     },
     {
       id: 9,
-      name: 'Ice Coffee',
+      name: 'Flat White',
       username: 'Delphine',
       Rating: '5',
-      img: 'https://www.tasteofhome.com/wp-content/uploads/2021/06/starbucks-discontinued-drinks-QT-1200x800-1.jpg'
+      img: 'https://globalassets.starbucks.com/assets/e9330b18ae524e69bdcbe97460d6f5bb.jpg'
     },
     {
       id: 10,
       name: 'Pink Drink',
       username: 'Moriah.Stanton',
       Rating: '5',
-      img: 'https://www.tasteofhome.com/wp-content/uploads/2021/06/starbucks-discontinued-drinks-QT-1200x800-1.jpg'
+      img: 'https://globalassets.starbucks.com/assets/0279f9c5fa5941d2a65dd183d7a0b386.jpg'
     }
   ];
   
