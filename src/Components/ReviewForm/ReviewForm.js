05fed3e6397545923b94189147d4bb7e0150c5e1
stephanie-yung <stephanie.yung43@myhunter.cuny.edu>
2021-10-31 20:56:47 -0400

    Added fields for onSubmit review form

--- src/Components/ReviewForm/ReviewForm.js
+++ src/Components/ReviewForm/ReviewForm.js
@@ -1,18 +1,26 @@
 import React from "react";
 import "./ReviewForm.css";
 import {FaStar} from "react-icons/fa";
+import axios from "axios";
+
+const BASE_URL = "https://brewers-backend.herokuapp.com";
+const headers = {
+    'Content-Type': 'application/x-www-form-urlencoded'
+ };
 
 const colors = {
     orange: "#FFBA5A",
     grey: "a9a9a9"
 }
 
-function ReviewForm(){
+function ReviewForm(id){
     const stars = Array(5).fill(0);
     const msg = ""
     const [currentValue, setCurrentValue] = React.useState(0);
     const [hoverValue, setHoverValue] = React.useState(undefined);
     const [msgValue, setMsgValue] = React.useState(msg);
+    const [emailValue, setEmailValue] = React.useState(msg);
+    const [drinkIDValue, setDrinkIDValue] = React.useState(msg);
 
     const handleClick = value =>{
         setCurrentValue(value)
@@ -26,6 +34,14 @@ function ReviewForm(){
         setHoverValue(undefined)
     };
 
+    const updateDrinkID = (value)=>{
+        setDrinkIDValue(value);
+    };
+
+    const updateEmail = (value)=>{
+        setEmailValue(value);
+    };
+
     const updateMsg = (value)=>{
         setMsgValue(value);
     };
@@ -34,9 +50,31 @@ function ReviewForm(){
         console.log(event.target.value);
     }
 
+    let post_review_submit = async() => {
+        let params = {
+            user_email: emailValue,
+            drink_id: "6175dcfea9a59c84906015db",
+            // drink_id: drinkIDValue,
+            comment: msgValue,
+            drink_rating: currentValue
+        };
+        const { data } = await axios.post(`${BASE_URL}/add-review`, params, headers);
+        console.log("addReview", data.data);
+
+    }
+
+
     return(
         <div style={styles.container}>
             <h1> Drink Review Form</h1>
+            <div className="" style={styles.textareaEmail}>
+                <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+                <input className="pa2 input-reset ba bg-transparent white w-100" type="email" name="email-address"  id="email-address" onChange={e => setEmailValue(e.target.value)}/>
+            </div>
+            <div className="" style={styles.textareaEmail}>
+                <label className="db fw6 lh-copy f6" htmlFor="email-address">Drink ID</label>
+                <input className="pa2 input-reset ba bg-transparent white w-100" type="email" name="email-address"  id="email-address" onChange={e => setDrinkIDValue(e.target.value)}/>
+            </div>
             <div style = {styles.stars}>
                 {stars.map((_, index) =>{
                     return(
@@ -60,11 +98,15 @@ function ReviewForm(){
                 style={styles.textarea}
                 onChange={e => setMsgValue(e.target.value)}
             />
-            <button className="mousepointer" style={styles.button}>Submit</button>
-            {/* <div>
+            <button className="mousepointer" style={styles.button} onClick={post_review_submit}>Submit</button>
+            <div>
+                <h3>Email: {emailValue}</h3>
+                <h3>ID: {drinkIDValue}</h3>
                 <h3>Star Rating: {currentValue}</h3>
                 <h3>Feedback: {msgValue}</h3>
-            </div> */}
+                {/* <h3>Email: {emailValue}</h3> */}
+
+            </div>
         </div>
     );
 };
@@ -82,6 +124,14 @@ const styles = {
         padding: 10,
         margin: "20px 0",
         minHeight: 100,
+    },
+    textareaEmail:{
+        // border: "1px solid #a9a9a9",
+        borderRadius: 5,
+        width: 500,
+        // padding: 10,
+        // margin: "20px 0",
+        minHeight: 75,
     },
     button:{
         border: "1px solid #a9a9a9",
