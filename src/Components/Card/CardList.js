dc6525054a47779395f9089230ae5eadf36644fb
Sony <1sony9905@gmail.com>
2021-11-12 12:48:11 -0500

    just changed naming

--- src/Components/Card/CardList.js
+++ src/Components/Card/CardList.js
@@ -6,7 +6,7 @@ const CardList = ({c}) => {
         <div>
             {
                 c.map((user, i) => {
-                    return <Card  img={c[i].img} name={c[i].name} username = {c[i].username}  id ={c[i].id} />
+                    return <Card  img={c[i].img} name={c[i].name} user_name = {c[i].user_name}  id ={c[i].id} />
                 })
             }
         </div>
