56052fa879b43cb215e0e627de8e2ab6685bf7fc
Sony <1sony9905@gmail.com>
2021-11-12 18:38:41 -0500

    added locations

--- src/Components/Locations/Locations.js
+++ src/Components/Locations/Locations.js
@@ -1,14 +1,21 @@
 import React, {Component} from 'react';
 import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import { GoogleMap, withScriptjs, withGoogleMap } from 'react-google-maps';
 
-class Locations extends Component{
-    render(){
+function Map() {
+    return (
+        <GoogleMap defaultZoom={10}
+        defaultCenter= {{ lat: 45, lng :-75}}/>
+    );
+}
+const WrappedMap = withScriptjs(withGoogleMap(Map));
+export default function Location() {
         return(
-            <div>
-                <h1 className="centerText">Location Page Test.</h1>
+            <div style={{  width:"80vh", height:'80vh'}}>
+                <WrappedMap googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyDeCr3XCNb-yyV1dxv4CCpkJzsLaJrp8FQ`} loadingElement={<div style={{ height: `100%` }} />}
+                            containerElement={<div style={{ height: `100%` }} />}
+                            mapElement={<div style={{ height: `100%` }} />}/>
             </div>
         );
     }
-}
 
-export default Locations;
