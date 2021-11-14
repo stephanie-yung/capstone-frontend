b44d0c6e194c7ca66ced876b695a3b6590bc1634
stephanie-yung <stephanie.yung43@myhunter.cuny.edu>
2021-11-13 20:25:58 -0500

    console log review

--- src/Components/SingleDrinkPage/ReviewDrinkList.js
+++ src/Components/SingleDrinkPage/ReviewDrinkList.js
@@ -2,11 +2,12 @@ import React from "react";
 import ReviewBox from "./ReviewDrinkBox"
 
 const ReviewList = ({review}) => {
+    console.log("REVIEWLIST PAGE COMPONENT: ",review[0]);
     return(
         <div>
             {
                 review.map((comment, i) => {
-                    return <ReviewBox key={i} comment={review[i].comment}/>
+                    return <ReviewBox key={i} comment={review[i]}/>
                 })
             }
         </div>
