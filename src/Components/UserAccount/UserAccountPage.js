848230374915a79cef8fdb113b488ee723032ad7
stephanie-yung <stephanie.yung43@myhunter.cuny.edu>
2021-11-19 15:41:04 -0500

    add user getdrinks / getreviews

--- src/Components/UserAccount/UserAccountPage.js
+++ src/Components/UserAccount/UserAccountPage.js
@@ -15,18 +15,65 @@ const UserAccount = () => {
     const [FirstName, setFirstName] = useState(0);
     const [LastName, setLastName] = useState(0);
     const [Email, setEmail] = useState(0);
+    const [ReviewsArray, setReviewsArray] = useState([])
+    const [DrinkName, setDrinkName] = useState(0);
+    const [DrinkIngredients, setDrinkIngredients] = useState(0);
+    var IngredientsMap = new Map();
+    var IngredientsList = [];
 
+    let drinksArray;
+    useEffect(async () => {
     let get_user = async(email) =>{
         email = "andy@gmail.com";
         const { data } = await axios.get(`${BASE_URL}/users/${email}`);
         console.log("USER DATAAAAA.DATAAAAA",data.data);
+        console.log("USER DATAAAAA.DATAAAAA.Drink-ids",data.data.drink_ids);
         // console.log("USER INFO",data.data.fname);
         setFirstName(data.data.fname);
         setLastName(data.data.lname);
         setEmail(data.data.email);
-
+        get_drinks(data.data.drink_ids);
+        get_reviews(data.data.review_ids);
     }
     get_user();
+
+    let get_drinks = async(drink_ids) => {
+        for(let i = 0; i < drink_ids.length ;i++) {
+            const { data } = await axios.get(`${BASE_URL}/drinks/${drink_ids[i]}`);
+            console.log(data);
+            const drinks = data.data;
+            setDrinkName(data.data.name);
+            setDrinkIngredients(data.data.ingredients);
+            // drinksArray.push(drinks);
+        }
+
+        console.log("/THIS IS THE DINRKS ARRAY INSIDER FUNC2: ", drinksArray);
+        // setDrinkReviewArray(reviewsArray);
+        // setReviewsLoaded(true);
+    }
+    
+    // for(let i = 0; i< DrinkIngredients.length; i++){
+    //     var ingredientsFull = DrinkIngredients[i][0]+": "+ DrinkIngredients[i][1]+". ";
+    //     IngredientsList.push(ingredientsFull);
+    //     console.log(IngredientsList)
+    // }
+    // console.log("Drink Ingredients Map", IngredientsMap);
+    // const IngredientsItems = IngredientsList.map((ingredient) =>
+    //     <li>{ingredient}</li>
+    // );
+
+    let get_reviews = async (review_ids) => {
+        let reviewsArray = [];
+        for (let i = 0; i < review_ids.length; i++) {
+            const { data } = await axios.get(`${BASE_URL}/reviews/${review_ids[i]}`)
+            const review = data.data;
+            reviewsArray.push(review)
+        }
+
+        setReviewsArray(reviewsArray)
+    }
+
+
 
     // let post_user = async() =>{
     //     let params = {
@@ -50,6 +97,7 @@ const UserAccount = () => {
 
     }
     //delete_review()
+    }, []);
 
     return (
        <div className="margin2">
